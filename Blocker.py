633e48f69d5c4ccad621454a705ef11bc1856997
SurgeTB <31511890+SurgeTB@users.noreply.github.com>
2018-10-28 22:26:01 +0000

    Update Blocker.py

--- Blocker.py
+++ Blocker.py
@@ -1,44 +1,30 @@
 import psutil
 import time
+import sys, subprocess
 
-number_machine_broke = 1
+subprocess.call([sys.executable, "-m", "pip", "install", "plyer"])
 
-while number_machine_broke != 0:
-    finish1 = input("Enter time to run for (e.g. 1s, 5m, 1h): ")
-    if finish1[-1:] == "s":
-        finish_final = int(finish1[:-1])
-        number_machine_broke = 0
-    elif finish1[-1:] == "m":
-        finish_final = int(finish1[:-1]) * 60
-        number_machine_broke = 0
-    elif finish1[-1:] == "h":
-        finish_final = int(finish1[:-1]) * 3600
-        number_machine_broke = 0
-    elif finish1[-1:] == "d":
-        finish_final = int(finish1[:-1]) * 86400
-        number_machine_broke = 0
+from plyer import notification
 
-print("\nRunning for", str(finish_final), "seconds...\n")
+block = ["toribash", "discord", "steam", "battle", "overwatch", "blizzard", "borderlands", "watch"] #add things to block here....
+last_not = 0
 
-block = ["discord", "toribash", "epic", "blizzard", "steam", "overwatch", "league", "lol", "battle"] #add things to block here....
-
-start = time.time()
-
-elapsed = 0
-
-while elapsed < finish_final:
-    time.sleep(2)
+while 1:
     for p in psutil.process_iter():
-        for check in block:
-            if check in (p.name()).lower():
+        time.sleep(0.01)
+        if p.name().lower()[:-4] in block:
+            try:
+
+                if time.time() - last_not > 3 or last_not == 0:
+                    notification.notify(title=(p.name()[:-4]+ " closed "), message=str(time.strftime("%H:%M")+" - Closed "+ p.name()), app_name="Python Blocker")
+                    
+                last_not = time.time()
+                p.kill()
                 print(time.strftime("%H:%M"), "- Closed", p.name())
-                try:
-                    p.kill()
-                except:
-                    print(time.strftime("%H:%M"), "- Failed to close", p.name())
-                break
+            except:
+                print(time.strftime("%H:%M"), "- Failed to close", p.name())
+                
+            
+            
 
-    elapsed = time.time() - start
-
-print("\nFinished!")
 
